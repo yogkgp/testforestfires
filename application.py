81097860d48e24ed9c925a0d93d2941accbfd723
yogkgp <ymali5334@gmail.com>
2026-06-18 10:20:42 +0530

    Added python.config

--- application.py
+++ application.py
@@ -18,7 +18,23 @@ def index():
 @app.route("/predictdata", methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method=="POST":
-        pass
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html', results=result[0])
+    
+
     else:
         return render_template('home.html')
 
